71cac1d9df791c58700d48f15f1957fbcc083b16
vinicius-leprevost <vinilep@hotmail.com>
2022-05-29 18:34:09 -0300

    Novas alterações Verificação por e-mail

--- webApp/php/cadastroPaciente.php
+++ webApp/php/cadastroPaciente.php
@@ -5,20 +5,37 @@ $nome = $_POST["ajax_nome"];
 $sobrenome = $_POST["ajax_sobrenome"];
 $email = $_POST["ajax_email"];
 
+$propriedades_cadastro = [ // CADASTRO PADRÃO FIREBASE
+  'email' => $email,
+  'emailVerified' => false,
+  'phoneNumber' => '+55'.$celular,
+  'password' => $pwd,
+  'displayName' => $pNome." ".$uNome,
+  'photoUrl' => '',
+  'disabled' => false,
+];
+
 $dados_cadastro = [ // CADASTRO PARA REALTIME
-    "Data_Nascimento" => $dataNascimento,
-    "Cpf" => $cpf,
-    'Primeiro_Nome' => $pNome,
-    'Ultimo_Nome' => $uNome,
-    'Display_Name' => $pNome." ".$uNome,
+  "Data_Nascimento" => $dataNascimento,
+  "Cpf" => $cpf,
+  'Primeiro_Nome' => $pNome,
+  'Ultimo_Nome' => $uNome,
+  'Display_Name' => $pNome." ".$uNome,
 ];
 
 
+// CADASTRO EM "AUTHENTICATION"
+$criarUsuario = $auth->createUser($propriedades_cadastro);
 
+// PEGAR UID
+$uid = $auth->getUserByEmail($email)->uid;
 
+// CADASTRO EM REALTIME "CONTAS"
+$updates = [
+  "PreCadastro/".$uid => $dados_cadastro,
+];
 
-
-
+$md5=md5();
 
 
 $link='http://localhost/projeto/OUTFLIX_empresa_atual_v11/php/confirmar_Email.php?usr='.$md5;
